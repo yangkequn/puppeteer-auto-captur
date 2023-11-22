2a83e7871cfe4f6d49aab786243cf93845c24510
yangkequn <yangkequn@gmail.com>
2023-11-22 18:27:01 +0800

    wait 3 second for the page to load

--- main.js
+++ main.js
@@ -240,6 +240,8 @@ async function main() {
         lastPage = null
       }
     })
+    //wait 3 seconds for the page to load
+    await page.waitFor(3000)
 
     try {
       const stream = await getStream(page, {
